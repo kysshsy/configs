9ce52a39fee8dfb4bffa6288a6f1fdc88ba2dd26
kysshsy <kysshsy@gmail.com>
2026-08-16 23:47:44 +0800

    feat(nixos): enable Bluetooth support

--- nix/hosts/nixos-niri/default.nix
+++ nix/hosts/nixos-niri/default.nix
@@ -11,6 +11,7 @@
 
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
+  hardware.bluetooth.enable = true;
 
   time.timeZone = "Asia/Shanghai";
   i18n.defaultLocale = "zh_CN.UTF-8";
