d02ead3288d8d97732636305006e3aff165192aa
kysshsy <kysshsy@gmail.com>
2026-08-22 11:48:43 +0800

    nixos: enable nix-ld

--- nix/modules/nixos/system.nix
+++ nix/modules/nixos/system.nix
@@ -39,6 +39,8 @@ in
 
   nixpkgs.config.allowUnfree = true;
 
+  programs.nix-ld.enable = true;
+
   environment.sessionVariables = {
     http_proxy = "http://127.0.0.1:7890";
     https_proxy = "http://127.0.0.1:7890";
